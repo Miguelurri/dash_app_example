0366fc8e55cfeeb57dc01bc081d9128c0d287276
Miguelurri <miguel.herraez02@gmail.com>
2019-12-23 01:39:47 +0100

    Update app.py
    Change to proper app

--- app.py
+++ app.py
@@ -5,63 +5,144 @@
 
 
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
-df = pd.read_csv(
-    'https://raw.githubusercontent.com/plotly/'
-    'datasets/master/gapminderDataFiveYear.csv')
-
-app = dash.Dash(__name__)
-server=app.server
+app = dash.Dash()
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+df = pd.read_csv('nama_10_gdp_1_Data.csv')
+available_items = df['NA_ITEM'].unique()
+available_locations = df['GEO'].unique()
+available_units = df['UNIT'].unique()
+available_time = df['TIME'].unique()
+
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
+    html.Div([
+        dcc.Dropdown(
+            id='xaxis-column-a',
+            options=[{'label': i, 'value': i} for i in available_items],
+            value='Gross domestic product at market prices'
+        )], style={'width': '48%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='yaxis-column-a',
+            options=[{'label': i, 'value': i} for i in available_items],
+            value='Value added, gross'
+        )], style={'width': '48%', 'display': 'inline-block'}),
+
+    html.Div([
+        dcc.RadioItems(
+            id='units-a',
+            options=[{'label': i, 'value': i} for i in available_units],
+            value='Chain linked volumes, index 2010=100'
+        )], style={'width': '55%', 'margin': 'auto'}),
+
+    dcc.Graph(id='output-a'),
+
+    html.Div([dcc.Slider(
+        id='year-slider-a',
+        min=df['TIME'].min(),
+        max=df['TIME'].max(),
+        value=df['TIME'].max(),
         step=None,
-        marks={str(year): str(year) for year in df['year'].unique()}
-    )
+        marks={str(year): str(year) for year in df['TIME'].unique()}
+    )], style={'width': '95%', 'margin': 'auto'}),
+
+    # SECOND GRAPH
+
+    html.Div([
+        dcc.Dropdown(
+            id='xaxis-column-b',
+            options=[{'label': i, 'value': i} for i in available_locations],
+            value='European Union - 28 countries'
+        )], style={'width': '48%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='yaxis-column-b',
+            options=[{'label': i, 'value': i} for i in available_items],
+            value='Value added, gross'
+        )], style={'width': '48%', 'display': 'inline-block'}),
+
+    html.Div([
+        dcc.RadioItems(
+            id='units-b',
+            options=[{'label': i, 'value': i} for i in available_units],
+            value='Chain linked volumes, index 2010=100'
+        )], style={'width': '55%', 'margin': 'auto'}),
+
+    dcc.Graph(id='output-b')
 ])
 
+
 @app.callback(
-    dash.dependencies.Output('graph-with-slider', 'figure'),
-    [dash.dependencies.Input('year-slider', 'value')])
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-    traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(go.Scatter(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
+    dash.dependencies.Output('output-a', 'figure'),
+    [dash.dependencies.Input('xaxis-column-a', 'value'),
+     dash.dependencies.Input('yaxis-column-a', 'value'),
+     dash.dependencies.Input('year-slider-a', 'value'),
+     dash.dependencies.Input('units-a', 'value')])
+def update_graph1(xaxis_column_name, yaxis_column_name,
+                  year_value, units_value):
+    dff = df[(df['TIME'] == year_value) & (df['UNIT'] == units_value)]
+    return {'data': [go.Scatter(
+            x=dff[dff['NA_ITEM'] == xaxis_column_name]['Value'],
+            y=dff[dff['NA_ITEM'] == yaxis_column_name]['Value'],
+            text=dff[dff['NA_ITEM'] == yaxis_column_name]['GEO'],
             mode='markers',
-            opacity=0.7,
             marker={
                 'size': 15,
+                'opacity': 0.5,
                 'line': {'width': 0.5, 'color': 'white'}
+            }
+            )], 'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear'
             },
-            name=i
-        ))
-
-    return {
-        'data': traces,
-        'layout': go.Layout(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
             hovermode='closest'
-        )
-    }
+            )
+            }
+
+
+@app.callback(
+    dash.dependencies.Output('output-b', 'figure'),
+    [dash.dependencies.Input('xaxis-column-b', 'value'),
+     dash.dependencies.Input('yaxis-column-b', 'value'),
+     dash.dependencies.Input('units-b', 'value')])
+def update_graph2(xaxis_column_name, yaxis_column_name, units_value):
+    dff = df[(df['GEO'] == xaxis_column_name) & (df['UNIT'] == units_value)]
+    return {'data': [go.Scatter(
+            x=dff[(dff['NA_ITEM'] == yaxis_column_name)]['TIME'],
+            y=dff[(dff['NA_ITEM'] == yaxis_column_name)]['Value'],
+            text=dff[dff['NA_ITEM'] == yaxis_column_name]['Value'],
+            mode='lines',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+            )], 'layout': go.Layout(
+            xaxis={
+                'title': 'Time',
+                'type': 'linear'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+            )
+            }
+
 
 if __name__ == '__main__':
-    app.run_server()
-
+    app.run_server(debug=False)
