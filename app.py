42b69dfa6dbd7bcd47290e0b518a333fbfa7ecc9
Miguelurri <miguel.herraez02@gmail.com>
2019-12-23 02:09:58 +0100

    added server to app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import plotly.graph_objs as go
 import pandas as pd
 
 app = dash.Dash()
+server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 df = pd.read_csv('data/nama_10_gdp_1_Data.csv')
